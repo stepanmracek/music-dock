a9d394574ca67924dd7cbb86c7320aeacd1ed115
Stepan Mracek <stepan.mracek@gmail.com>
2019-07-30 13:39:59 +0200

    api: add CORS

--- api/src/init.py
+++ api/src/init.py
@@ -2,10 +2,12 @@ from flask import Flask, g
 from models import db
 from redis_client import create_client
 from database import DATABASE_CONNECTION_URI
+from flask_cors import CORS
 
 
 def create_app(name):
     app = Flask(name)
+    CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.app_context().push()
